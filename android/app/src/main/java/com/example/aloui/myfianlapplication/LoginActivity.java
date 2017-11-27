b31cf48a2071ccfcec6ec258e11bce688ea7654a
hamza <hamza.aloui@esprit.tn>
2017-11-28 00:54:34 +0100

    un autre fix test

--- android/app/src/main/java/com/example/aloui/myfianlapplication/LoginActivity.java
+++ android/app/src/main/java/com/example/aloui/myfianlapplication/LoginActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
     private Button LoginButton,RegisterButton,ForgetPassword;
